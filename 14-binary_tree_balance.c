a2bfbff1e48934a1556fc05be50644dc57af9671
JamesMathenge <mathengejames64@gmail.com>
2023-08-31 23:14:11 +0000

    task 14

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -7,7 +7,7 @@
  *
  * Return: Height of the tree, 0 if tree is NULL
  */
-size_t binary_tree_height(const binary_tree_t *tree)
+size_t binary_tree_height_a(const binary_tree_t *tree)
 {
 	size_t left_height, right_height;
 
