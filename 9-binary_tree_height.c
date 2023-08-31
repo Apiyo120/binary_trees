ae6a8653841d4fe0eb8c8e75a0131fb44c304ef1
JamesMathenge <mathengejames64@gmail.com>
2023-08-31 22:53:05 +0000

    task 9

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -9,13 +9,13 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t height_l = 0;
-	size_t height_r = 0;
+	size_t left_height, right_height;
 
-	if (!tree)
-		return (0);
+	if (tree == NULL)
+		return (-1);
 
-	height_l = tree->left ? 1 + binary_tree_height(tree->left) : 0;
-	height_r = tree->right ? 1 + binary_tree_height(tree->right) : 0;
-	return (height_l > height_r ? height_l : height_r);
+	left_height = binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
+
+	return (1 + (left_height > right_height ? left_height : right_height));
 }
